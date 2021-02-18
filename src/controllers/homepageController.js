d0722bc30b6ca144250ded71642e3caabba685ec
Emer Rocks <Rocks-E2@ulster.ac.uk>
2021-02-18 20:30:14 +0000

    get methods for login and register pages

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -14,8 +14,42 @@ const createNewUser = async (req, res) => {
   console.log('This is create new user', message)
   return res.redirect('/')
 }
+
+const getLoginPage = (req, res) => {
+  return res.render("auth/login.ejs")
+}
+
+const getRegisterPage = (req, res) => {
+  return res.render("auth/register.ejs")
+}
+
+//create new user for the database
+const handleRegister = async (req, res) => {
+  //validate input fields
+
+  //create a new user
+  try {
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword,
+      createdAt: Date.now()
+    }
+    const message = await userService.createNewUser(user)
+    console.log(message)
+    return res.redirect("/")
+  }catch (e) {
+    console.log(e)
+  }
+}
+
 module.exports = {
   getHomepage: getHomepage,
   getNewUserPage: getNewUserPage,
   createNewUser: createNewUser,
+  getRegisterPage: getRegisterPage,
+  getLoginPage: getLoginPage,
+  handleRegister: handleRegister,
 }
