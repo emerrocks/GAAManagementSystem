5a0be9a7e084f9a8184330f606a753719daa8928
Emer Rocks <Rocks-E2@ulster.ac.uk>
2021-02-18 20:31:01 +0000

    new routes created for register and login

--- src/routes/web.js
+++ src/routes/web.js
@@ -5,6 +5,10 @@ const router = express.Router()
 
 const initAllWebRoutes = (app) => {
   router.get('/', homepageController.getHomepage)
+  router.get('/register', homepageController.getRegisterPage)
+  router.get('/login', homepageController.getLoginPage)
+
+  router.post("/register", homepageController.handleRegister)
   router.get('/new-user', homepageController.getNewUserPage)
   router.post('/create-new-user', homepageController.createNewUser)
   return app.use('/', router)
